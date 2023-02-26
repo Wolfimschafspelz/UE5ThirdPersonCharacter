4394a7e44ac9fb7008c7b152f1ecbdf759ee35b0
Lukas Steinmann <lukas.steinmann@gmx.de>
2023-02-26 02:05:19 +0100

    Initial Commit

---
+++ Source/FirstGame/Private/Mount.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mount.h"
+
+// Add default functionality here for any IMount functions that are not pure virtual.
